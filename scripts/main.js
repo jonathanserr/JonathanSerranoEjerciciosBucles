b89b4a14a7b166b40bc1e25ae983858e68afa35b
jonathan_serrano <jonathan_serrano@hotmail.com>
2024-07-20 01:16:17 -0500

    termine ejercicios falta arreglos esteticos

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,14 @@
 // // 1
 // alert ("EJERCICIO 1:"+"\n"+"TABLA DE MULTIPLICAR:");
-// let numero = parseInt(prompt(" Ingrese Numero a MULTIPLICAR:"));
+// let numero0 = parseInt(prompt(" Ingrese Numero a MULTIPLICAR:"));
 // let total = null;
 // for (let i = 1; i <= 10; i++) {
-//     total = numero * i;
-//     console.log(numero+" * "+i+" = "+ total);
+//     total = numero0 * i;
+//     console.log(numero0+" * "+i+" = "+ total);
 // }
 
-// 2
+// // 2
+// alert ("EJERCICIO 2:"+"\n"+"NUMEROS ACUMULADOS")
 // let numeros =[];
 // let numeroAcumulado;
 // do {
@@ -15,7 +16,7 @@
 //     if (numeroAcumulado !==0) {
 //         numeros.push(numeroAcumulado);
 //     }
-    
+
 // } while (numeroAcumulado !== 0);
 
 // console.log("Los numeros acumulados son: "+numeros.join(", "));
@@ -39,23 +40,33 @@
 
 
 
-//4
+// //4 Utilizo funcion encontrarDivisores que hice para el siguiente ejercicio
+// let primo = parseInt(prompt(" Ingrese Numero a verificar si es PRIMO :"));
+// let divisores1 = encontrarDivisores(primo);
+// console.log(divisores1.length);
+// if (divisores1.length <= 2 && primo >= 2) {
+//     console.log("PRIMO");
+// } else {
+//     console.log("No PRIMO");
+// }
+// console.log(divisores1);
 
-
-//5
+// //5
+// alert("EJERCICIO 5:" + "\n" + "DIVISORES DE UN NUMERO")
 // function encontrarDivisores(numero) {
-//     let divisores =[];
-//     for (let i = 0; i <= numero; i++) {
+//     let divisores = [];
+//     for (let i = 1; i <= numero; i++) {
 //         if (numero % i === 0) {
-//            divisores.push(i) 
+//             divisores.push(i)
 //         }
 //     }
 //     return divisores;
 // }
 
-// let numero = parseInt(prompt(" Ingrese Numero"));
-// let divisores = encontrarDivisores(numero);
-// console.log("Los DIVISORES del numero "+numero+" son: "+divisores.join(", "));
+// let numero1 = parseInt(prompt(" Ingrese Numero"));
+// let divisores = encontrarDivisores(numero1);
+// console.log(divisores);
+// console.log("Los DIVISORES del numero " + numero1 + " son: " + divisores.join(", "));
 
 // // 6
 // alert ("EJERCICIO 6:"+"\n"+"ARREGLO 10 POSICIONES X CONSOLA");
@@ -72,10 +83,47 @@
 // }
 
 
-// 8
+// // 8
 
-// 9
+let arrayFamilia = [
+    {
+        nombre: "María Teresa",
+        edad: 62,
+        parentesco: "Madre",
+        ocupacion: "Pensionada"
+    },
+    {
+        nombre: "Fernado",
+        edad: 63,
+        parentesco: "Padre",
+        ocupacion: "Ingeniero"
+    },
+    {
+        nombre: "Jonathan",
+        edad: 41,
+        parentesco: "Hijo",
+        ocupacion: "Desarrollador Web"
+    },
+    {
+        nombre: "Nicolas",
+        edad: 35,
+        parentesco: "Hijo",
+        ocupacion: "Administrador"
+    },
+    {
+        nombre: "Santiago",
+        edad: 29,
+        parentesco: "Hijo",
+        ocupacion: "Bacteriologo"
+    }
+];
 
+for (let i = 0; i < arrayFamilia.length; i++) {
+    let persona = arrayFamilia[i];
+    console.log("Hola mi nombre es:  " + persona.nombre + ", tengo " + persona.edad + " años, soy " + persona.parentesco + " y mi ocupacion es " + persona.ocupacion);
+}
+// // 9
+// alert ("EJERCICIO 9:"+"\n"+"ARREGLO 10 NUMEROS MUESTRA LOS IMPARES")
 // let array2 = [1,2,3,4,5,6,7,8,9,10]
 // let arrayImpares = [];
 // for (let i = 0; i < array2.length; i++) {
@@ -85,7 +133,8 @@
 // }
 // console.log(arrayImpares);
 
-// 10
+// // 10
+// alert ("EJERCICIO 10:"+"\n"+"INGRESO NUMEROS PARA SUMAR LOS PARES Y LOS IMPARES")
 // let numero;
 // let sumaPar = 0;
 // let sumaImpar = 0;
@@ -102,17 +151,19 @@
 // console.log("La suma de los IMPARES es: "+sumaImpar);
 
 // //11
+// alert ("EJERCICIO 11:"+"\n"+"ARREGLO 10 NUMEROS MUESTRA EL MAYOR")
 // let array3 = [10,7,3,99,55,6,2,55,9,1];
 // let mayor = array3[0];
 // for (let i = 0; i < array3.length; i++) {
 //     if (mayor <= array3[i]) {
 //         mayor = array3[i]
-        
+
 //     }
 // }
 // console.log(mayor);
 
-//12
+// //12
+// alert ("EJERCICIO 12:"+"\n"+"ARREGLO 10 NUMEROS MUESTRA EL MENOR")
 // let array4 = [10,-7,0,0,-55,6,2,55,9,-3];
 // let menor = array4[0];
 // for (let i = 9; i >= 0; i--) {
@@ -123,54 +174,52 @@
 // console.log(menor);
 
 
-// 13
-alert("EJERCICIO 13:"+"\n"+"PIEDRA, PAPEL O TIJERAS")
-let player1 = {
-    nombre : null,
-    jugada : null,
-}
-let player2 = {
-    nombre : null,
-    jugada : null,
-}
+// // 13
+// alert("EJERCICIO 13:"+"\n"+"PIEDRA, PAPEL O TIJERAS")
+// let player1 = {
+//     nombre : null,
+//     jugada : null,
+// }
+// let player2 = {
+//     nombre : null,
+//     jugada : null,
+// }
 
-player1.nombre = prompt("Ingrese NOMBRE PLAYER 1");
-player2.nombre = prompt("Ingrese NOMBRE PLAYER 2");
-do { 
-    player1.jugada = prompt("PLAYER " + player1.nombre + "\n" +"piedra/papel/tijeras");
-    player2.jugada = prompt("PLAYER " + player2.nombre + "\n" +"piedra/papel/tijeras");
-    if (player1.jugada ==="piedra") {
-        if (player2.jugada === "tijeras") {
-            console.log("GANA : "+player1.nombre);
-            break;
-        } else if(player2.jugada ==="papel"){
-            console.log("GANA : "+player2.nombre);
-            break;
-        }
-
-
-    } else if (player1.jugada ==="papel") {
-        if (player2.jugada === "piedra") {
-            console.log("GANA : "+player1.nombre);
-            break;
-        } else if(player2.jugada ==="tijeras"){
-            console.log("GANA : "+player2.nombre);
-            break;
-        }
-        
-    } else if (player1.jugada ==="tijeras") {
-        if (player2.jugada === "papel") {
-            console.log("GANA : "+player1.nombre);
-            break;
-        } else if(player2.jugada ==="piedra"){
-            console.log("GANA : "+player2.nombre);
-            break;
-        }
-    }
-} while (player1.jugada === player2.jugada);
+// player1.nombre = prompt("Ingrese NOMBRE PLAYER 1");
+// player2.nombre = prompt("Ingrese NOMBRE PLAYER 2");
+// do { 
+//     player1.jugada = prompt("PLAYER " + player1.nombre + "\n" +"piedra/papel/tijeras");
+//     player2.jugada = prompt("PLAYER " + player2.nombre + "\n" +"piedra/papel/tijeras");
+//     if (player1.jugada ==="piedra") {
+//         if (player2.jugada === "tijeras") {
+//             console.log("GANA : "+player1.nombre);
+//             break;
+//         } else if(player2.jugada ==="papel"){
+//             console.log("GANA : "+player2.nombre);
+//             break;
+//         }
+//     } else if (player1.jugada ==="papel") {
+//         if (player2.jugada === "piedra") {
+//             console.log("GANA : "+player1.nombre);
+//             break;
+//         } else if(player2.jugada ==="tijeras"){
+//             console.log("GANA : "+player2.nombre);
+//             break;
+//         }    
+//     } else if (player1.jugada ==="tijeras") {
+//         if (player2.jugada === "papel") {
+//             console.log("GANA : "+player1.nombre);
+//             break;
+//         } else if(player2.jugada ==="piedra"){
+//             console.log("GANA : "+player2.nombre);
+//             break;
+//         }
+//     }
+// } while (player1.jugada === player2.jugada);
 
 
 // // 14
+// alert("EJERCICIO 14:"+"\n"+"TRIANGULO 5 ASTERISCOS DE LADO")
 // for (let x = 1; x <= 5; x++) {
 //     let asterisco = "";
 //     for (let y = 1; y <= x; y++) {
@@ -180,7 +229,8 @@ do {
 //     console.log(asterisco);
 // }
 
-// 15
+// // 15
+// alert("EJERCICIO 15:"+"\n"+"TRIANGULO 5 ASTERISCOS DE LADO INVERTIDO")
 // for (let x = 5; x >= 1; x--) {
 //     let asterisco = "";
 //     for (let y = 1; y <= x; y++) {
@@ -192,28 +242,25 @@ do {
 
 
 // for (let x = 5; x >= 1; x--) {
-    
+
 //     console.log('* '.repeat(x));
 // }
 
 
 
 // 16
-// alert ("EJERCICIO 16:"+"\n"+"ARREGLO 10 NUMEROS DESORDENADOS--- DEVUELVE X CONSOLA ARREGLO ORDENADO");
-// let array8 = [5,3,0,1,9,2,6,7,4,8]
-// let temp = null;
-// for (let paso = 1; paso < array8.length; paso++) {
-//     for (let i = 0; i < array8.length -1; i++) {
-//         if (array8[i] > array8[i+1]) { 
-//             temp = array8[i];
-//             array8[i] = array8[i+1];
-//             array8[i+1] = temp
-//         }
-        
-//     };
-// }
-// alert(array8)
-// console.table(array8)
-// for (let i = 0; i < array8.length; i++) {
-//     console.log(array8[i]);   
-// }
+alert("EJERCICIO 16:" + "\n" + "ARREGLO 10 NUMEROS DESORDENADOS--- DEVUELVE X CONSOLA ARREGLO ORDENADO");
+let array8 = [9, 8, 7, 6, 9, 4, 3, 2, 1, 9]
+let temp = null;
+for (let paso = 1; paso < array8.length; paso++) {
+    for (let i = 0; i < array8.length - 1; i++) {
+        if (array8[i] > array8[i + 1]) {
+            temp = array8[i];
+            array8[i] = array8[i + 1];
+            array8[i + 1] = temp
+        }
+
+    };
+}
+alert(array8)
+console.table(array8)
